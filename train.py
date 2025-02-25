ccf5791283cadc14a5668c4ce4594d480991adc9
Filipe Valente <65114635+lipivalente1@users.noreply.github.com>
2025-02-24 23:03:08 -0300

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO, settings
 
+#escolha um novo local para armazenar os resultados. Ou comenta para salvar no local padrão
 settings.update({'runs_dir': 'C:/Users/Filipe/Desktop/Projeto 1 - IC/results/runs'})
-
 
 model = YOLO('yolo11m.pt')
 model.model
 
 if __name__ == '__main__':
-    model.train(data='velhinhos.yaml', 
+    model.train(data='monitoramento_idosos.yaml', 
                 epochs=50,
                 batch=16,
                 seed=42,
