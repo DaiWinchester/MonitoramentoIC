df05a4d2bf039b06d976b6bfab7739d16d386b54
Dai Ribeiro <81192710+DaiWinchester@users.noreply.github.com>
2025-02-24 22:54:27 -0300

    Update validation.py

--- validation.py
+++ validation.py
@@ -3,15 +3,12 @@ model = YOLO('C:/Users/Filipe/Desktop/Projeto 1 - IC/results/runs/detect/train5/
 
 
 if __name__ == '__main__':
-    results = model.val(  # project='runs/detect',
+    results = model.val( 
         imgsz=640,
         batch=16,
         conf=0.001,
         iou=0.7,  # Non-Maximum Supression (NMS)
-        save_json=False,  # Save to JSON {image_id, cls, bbox, conf} of each image in dataset
-        save_hybrid=False,
-        # Bounding box labels + inference on the output image (Ultralytics 8.0.178 = results are wrong when this argmunent is True)
-        split='test')  # train, val or test
+        split='test')  # train, val ou test
 
     # results = model.val()
     print(results)
